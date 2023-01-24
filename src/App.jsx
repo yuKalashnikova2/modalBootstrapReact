c5ef2e5d7e07cde3482779901dae3a23249183dc
YuliyaKalashnikova <pmalian@list.ru>
2023-01-24 11:21:51 +0700

    feat(Modal)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Container from './components/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Button from './components/Button';
+import Button from './components/Button'
+import Modal from './components/Modal'
 
 export function App() {
   return (
     <Container>
       <Header></Header>
       <Button data-bs-toggle="modal" data-bs-target="#exampleModal">Открыть модальное окно</Button>
+      <Modal id="exampleModal" />
       <Footer></Footer>
     </Container>
 
