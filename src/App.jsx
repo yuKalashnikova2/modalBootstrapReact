aba87aa486eefa9d6b3e8f74eceb4d0cd829c846
YuliyaKalashnikova <pmalian@list.ru>
2023-01-24 12:53:28 +0700

    feat(Form); fix Modal

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Button from './components/Button'
 import Modal from './components/Modal'
+import Form from './components/Form'
 
 export function App() {
   return (
     <Container>
       <Header></Header>
       <Button data-bs-toggle="modal" data-bs-target="#exampleModal">Открыть модальное окно</Button>
-      <Modal id="exampleModal" />
+      <Modal id="exampleModal">
+        <Form />
+      </Modal>
       <Footer></Footer>
     </Container>
 
---
+++ src/components/Form.jsx
@@ -0,0 +1,63 @@
+const Form = (props) => {
+  return (
+    <form>
+      <div className="mb-4">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Email address
+        </label>
+        <input
+          type="email"
+          className="form-control"
+          id="exampleInputEmail1"
+          aria-describedby="emailHelp"
+          placeholder="Enter e-mail"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Password
+        </label>
+        <input
+          type="password"
+          className="form-control"
+          id="exampleInputPassword1"
+          placeholder="Enter password"
+        />
+      </div>
+      <div className="mb-4 fs-small">
+        <div className="row">
+          <div className="col-6">
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Keep me sign in
+              </label>
+            </div>
+          </div>
+          <div className="col-6 text-end">
+            <a href="#" className="link-primary">
+              Forgot password?
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="d-grid">
+        <button type="submit" className="btn btn-primary btn-lg">
+          Submit
+        </button>
+      </div>
+      <div className="mt-4 text-muted fs-small">
+        Don’t have an account?
+        <a href="#" className="link-primary">
+          Sign up now
+        </a>
+      </div>
+    </form>
+  )
+}
+
+export default Form
