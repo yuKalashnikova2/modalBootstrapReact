8010243072ab09aab4f25ef7724f6dfaa5e93ce8
YuliyaKalashnikova <pmalian@list.ru>
2023-01-25 12:23:26 +0700

    feat: form logic

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
+
 const Form = (props) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [keepMe, setKeepMe] = useState(false)
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    console.log({
+      email,
+      password,
+      keepMe
+    })
+    
+
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-4">
         <label htmlFor="exampleInputEmail1" className="form-label">
           Email address
@@ -11,6 +29,8 @@ const Form = (props) => {
           id="exampleInputEmail1"
           aria-describedby="emailHelp"
           placeholder="Enter e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
       </div>
       <div className="mb-4">
@@ -22,6 +42,8 @@ const Form = (props) => {
           className="form-control"
           id="exampleInputPassword1"
           placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
       <div className="mb-4 fs-small">
@@ -32,6 +54,8 @@ const Form = (props) => {
                 type="checkbox"
                 className="form-check-input"
                 id="exampleCheck1"
+                checked={keepMe}
+                onChange={(e) => setKeepMe(e.target.checked)}
               />
               <label className="form-check-label" htmlFor="exampleCheck1">
                 Keep me sign in
